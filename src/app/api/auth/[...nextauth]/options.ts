906665313b4903dcf8166de6ce1c971c096d112a
Gabriel Godoy <gabyreload@gmail.com>
2023-12-21 16:02:27 -0300

    First commit Config Next auth and get data from api with jwt

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,58 @@
+import axios from "axios";
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+interface UserLogin {
+  _id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  code_technical: string;
+  role: string;
+  api_version: string;
+  imageUrl: string;
+}
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email:",
+          type: "email",
+          placeholder: "Email",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "",
+          email: credentials?.email,
+          password: credentials?.password,
+        };
+        const path = "http://localhost:3400/api/users/login";
+        const response = await axios.post(path, user);
+
+        if (response?.data?.status === "success") {
+          return response.data.payload.user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ account, token, user, profile, session }) {
+      if (user) token.user = user;
+      return token;
+    },
+    session({ session, token }) {
+      session.user = token.user as UserLogin;
+      return session;
+    },
+  },
+};
